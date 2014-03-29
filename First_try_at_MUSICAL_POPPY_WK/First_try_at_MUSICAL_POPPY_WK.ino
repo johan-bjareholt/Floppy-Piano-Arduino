9649ba7b3dbc24965df836d6774790372ac1e45b
Ga22be <ga22be@live.se>
2014-03-29 22:07:50 +0100

    Modified existing Arduino file
    Started adding multiplexing support

--- First_try_at_MUSICAL_POPPY_WK/First_try_at_MUSICAL_POPPY_WK.ino
+++ First_try_at_MUSICAL_POPPY_WK/First_try_at_MUSICAL_POPPY_WK.ino
@@ -26,11 +26,9 @@ boolean firstRun = true; // Used for one-run-only stuffs;
 // First pin being used for floppies, and the last pin.  Used for looping over all pins.
 const byte FIRST_PIN = 2;
 const byte PIN_MAX = 17;
-const byte potPin = A0;
 #define RESOLUTION 40 // Microsecond resolution for notes. Upplösningen.
 
-int potVal = 0;
-int potMap = 0;
+short assigned[] = {0,0,-1,0,-1,0,-1,0,-1,0,-1,0,-1,0,-1,0,-1,0};
 
 /*NOTE: Many of the arrays below contain unused indexes.  This is 
  to prevent the Arduino from having to convert a pin input to an alternate
@@ -40,11 +38,10 @@ int potMap = 0;
 
 //Array containing all periods we are interested in using. 0-48
 const int microPeriods[] = {
-        0,
         30578, 28861, 27242, 25713, 24270, 22909, 21622, 20409, 19263, 18182, 17161, 16198, //C1 - B1
         15289, 14436, 13621, 12856, 12135, 11454, 10811, 10205, 9632, 9091, 8581, 8099, //C2 - B2
         7645, 7218, 6811, 6428, 6068, 5727, 5406, 5103, 4816, 4546, 4291, 4050, //C3 - B3
-        3823, 3609, 3406, 3214, 3034, 2864, 2703, 2552, 2408, 2273, 2146, 2025, //C4 - B4
+        3823, 3609, 3406, 3214, 3034, 2864, 2703, 2552, 2408, 2273, 2146, 2025 //C4 - B4        
 };
 
 /*An array of maximum track positions for each step-control pin.  Even pins
@@ -77,11 +74,20 @@ unsigned int currentTick[] = {
   0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 
 };
 
+//Stuffs for multiplexing
+int bit0 = 0;
+int bit1 = 0;
+int bit2 = 0;
+int bit3 = 0;
 
+int count = 0;
+short values[] = {1,1,1,1, 1,1,1,1, 1,1,1,1, 1,1,1,1};
+short prevValues[] = {1,1,1,1, 1,1,1,1, 1,1,1,1, 1,1,1,1};
+
+boolean changed = false;
 
 //Setup pins (Even-odd pairs for step control and direction
 void setup(){
-  pinMode(13, OUTPUT);// Pin 13 has an LED connected on most Arduino boards
   pinMode(2, OUTPUT); // Step control 1
   pinMode(3, OUTPUT); // Direction 1
   pinMode(4, OUTPUT); // Step control 2
@@ -91,7 +97,12 @@ void setup(){
   pinMode(8, OUTPUT); // Step control 4
   pinMode(9, OUTPUT); // Direction 4
   
-  pinMode(potPin, INPUT); // Potentiometer pin
+  pinMode(A0, OUTPUT); // Control 1 for multiplexing
+  pinMode(A1, OUTPUT); // Control 2 for multiplexing
+  pinMode(A2, OUTPUT); // Control 3 for multiplexing
+  pinMode(A3, OUTPUT); // Control 4 for multiplexing
+ 
+  pinMode(13, INPUT_PULLUP); // Input from multiplexer
 
   Timer1.initialize(RESOLUTION); 
   /* 
@@ -122,11 +133,33 @@ void loop(){
     delay(2000);
   }
   
-  potVal = analogRead(potPin);
-  potMap = constrain(map(potVal, 0, 1023, 0, 48), 0, 48);
-  //Serial.println(potMap);
+  readKeyes();
   
-  currentPeriod[2] = (microPeriods[potMap])/(RESOLUTION*2);
+  if(changed){
+   for(int i = 0; i <= 15; i++){
+    if(values[i] == 0){
+     for(int j = 2; j <= 8; j+2){
+      if(assigned[j] < 0){
+       currentPeriod[j] = (microPeriods[i])/(RESOLUTION*2);
+       assigned[j] = i;
+       j = 9;
+      }
+     }
+    }   
+   
+    else if(values[i] == 1){
+     for(int j = 2; j <= 8; j+2){
+      if(assigned[j] == i){
+       currentPeriod[j] = 0;
+       assigned[j] = -1;
+       j = 9;
+     }      
+    }
+   }
+  } 
+ }
+} 
+  
    /*
    Current period assigned to each pin.  0 = off.  Each period is of the length specified by the RESOLUTION
    variable above.  i.e. A period of 10 is (RESOLUTION x 10) microseconds long.
@@ -135,7 +168,7 @@ void loop(){
    Var period definieras av upplösningen(RESOLUTION) tidigare definierad som 40 uS. 
    Alltså är en period om 10: (RESOLUTION*10)= 400uS lång.
    */
-}
+
 
 /*
 Called by the timer inturrupt at the specified resolution.
@@ -151,7 +184,7 @@ void tick()
    Om det finns en period definierad för pin 2: 
    Räkna hur många ticks som har passera, och toggla pin 2 om nuvarande period är uppnådd.
    */
-  for(int i = 2; i < 9; i+2){
+  for(int i = 2; i <= 8; i+2){
    if (currentPeriod[i]>0){
      currentTick[i]++;
      if (currentTick[i] >= currentPeriod[i]){
@@ -186,6 +219,33 @@ void togglePin(byte pin, byte direction_pin) {
   //Pulse the control pin
   digitalWrite(pin,currentState[pin]);
   currentState[pin] = ~currentState[pin];
+}
+
+void readKeyes(){
+  for(int i = 0; i<=15; i++){
+    prevValues[i] = values[i];
+  }
+  changed = false;
+  
+  for(count = 0; count<=15; count++){
+  
+  bit0 = bitRead(count, 0);
+  bit1 = bitRead(count, 1);
+  bit2 = bitRead(count, 2);
+  bit3 = bitRead(count, 3);
+  
+  digitalWrite(A0, bit0);
+  digitalWrite(A1, bit1);
+  digitalWrite(A2, bit2);
+  digitalWrite(A3, bit3);
+  
+  values[count] = digitalRead(13);
+  
+  if(values[count] != prevValues[count]){
+    changed = true; 
+  }
+  }
+delay(5);  
 }
 
 
