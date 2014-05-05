03e9380961d49771e772de71a1adb395f285fee1
Ga22be <ga22be@live.se>
2014-05-05 08:20:51 +0200

    Merging diffrent codes
    Excluding connection between buttons and floppies. Rest is worked out!

--- src/poppy/poppy.ino
+++ src/poppy/poppy.ino
@@ -7,12 +7,13 @@ boolean prevValues[] = {1,1,1,1, 1,1,1,1};
 short assigned[] = {0,0,-1,0,-1,0,-1,0,-1,0,-1,0,-1,0,-1,0,-1,0};
 
 void setup(){
+ // Multiplexer control pins
  pinMode(A0, OUTPUT);
  pinMode(A1, OUTPUT);
  pinMode(A2, OUTPUT);
- //pinMode(A3, OUTPUT);
- 
+ // Multiplexer read pin
  pinMode(13, INPUT_PULLUP);
+ 
  Serial.begin(9600);
 }
 
@@ -28,15 +29,16 @@ void loop(){
           // If floppy not busy
           if (assigned[activationPin] < 0){
             // Set tone to activation pin
-            //currentPeriod[activationPin] = (microPeriods[btnNum])/(RESOLUTION*2);
+            //currentPeriod[activationPin] = (microPeriods[keyNum])/(RESOLUTION*2);
+            Serial.write(char(keyNum));
             // Memorize button
             assigned[activationPin] = keyNum;
             //Serial.print("button ");
             //Serial.print(keyNum);
             //Serial.println(" is pressed");
-            Serial.print(assigned[activationPin]);
-            Serial.print(" to: ");
-            Serial.println(activationPin);
+            //Serial.print(assigned[activationPin]);
+            //Serial.print(" to: ");
+            //Serial.println(activationPin);
             // Stop loop
             activationPin = 9;
           }
@@ -47,12 +49,13 @@ void loop(){
           if(assigned[activationPin] == keyNum){
             // Stop floppy
             //currentPeriod[activationPin] = 0;
+            Serial.write(char(keyNum+(12*3)));
             //Serial.print("button ");
             //Serial.print(keyNum);
             //Serial.println(" is released");
-            Serial.print(assigned[activationPin]);
-            Serial.print(" from: ");
-            Serial.println(activationPin);       
+            //Serial.print(assigned[activationPin]);
+            //Serial.print(" from: ");
+            //Serial.println(activationPin);       
             // Set floppy floppy as unused
             assigned[activationPin] = -1;
             // Stop loop
@@ -65,9 +68,9 @@ void loop(){
   
   if(changed){
     for(int i = 0; i<=7; i++){
-      Serial.print(values[i]); 
+      //Serial.print(values[i]); 
     }
-    Serial.println("");
+    //Serial.println("");
   }
 }
 
